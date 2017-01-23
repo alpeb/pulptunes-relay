abe106d4fccd7eadbd3fcbbb6d78aaada16f268f
Alejandro Pedraza <alejandro.pedraza@gmail.com>
2017-01-23 13:23:48 -0500

    undid some evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -16,6 +16,8 @@ CREATE TABLE `licenses` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `licenses` (`id`, `subdomain`, `name`, `email`, `signup`, `trial`, `trial_ends`, `password`, `password_token`, `password_token_expiration`) VALUES (1, 'pulp', 'John', 'pulp@mailinator.com',  '2016-05-17 14:10:57',  0,  '2016-05-24 14:10:57',  '1000:7dc653160f4e5881b1f9d74ee24255996081f100a89f4649:70fffa9a0f00f4adbea790b168cee9d7037b0dbc9629b5e3', NULL, NULL);
+
 CREATE TABLE `servers` (
   `id` varchar(100) NOT NULL,
   `status` varchar(20) NOT NULL,
@@ -36,7 +38,9 @@ CREATE TABLE `serving_listeners` (
   CONSTRAINT `serving_listeners_ibfk_1` FOREIGN KEY (`server_id`) REFERENCES `servers` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `servers` (`id`, `status`, `public_dns`, `online`, `online_test`) VALUES ('pulp1',  'test', 'pulp1.localhost.localdomain:9000', 'true',  'false');
+INSERT INTO `servers` (`id`, `status`, `public_dns`, `online`, `online_test`) VALUES
+  ('pulp1',  'test', 'pulp1.localhost.localdomain:9000', 'true',  'false'),
+  ('pulp2', 'test', 'pulp2.localhost.localdomain:9001',  'false', 'false');
 
 CREATE TABLE `subdomain_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
